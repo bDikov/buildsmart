467f45522b3b76e8f74c5dd9b0a77ab31f06ad76
Boncho <bonchodikov@gmail.com>
2026-07-05 17:14:33 +0300

    indexing

--- BuildSmart.Web/Program.cs
+++ BuildSmart.Web/Program.cs
@@ -222,6 +222,47 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddAdditionalAssemblies(typeof(BuildSmart.SharedUI.Components.Layout.MainLayout).Assembly);
 
+app.MapGet("/sitemap.xml", async (HttpContext context) =>
+{
+    context.Response.ContentType = "application/xml";
+    
+    var sitemapXml = $@"<?xml version=""1.0"" encoding=""UTF-8""?>
+<urlset xmlns=""http://www.sitemaps.org/schemas/sitemap/0.9"">
+    <url>
+        <loc>https://buildsmart.bg/</loc>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>https://buildsmart.bg/feed</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.9</priority>
+    </url>
+    <url>
+        <loc>https://buildsmart.bg/faq</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>https://buildsmart.bg/terms</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.3</priority>
+    </url>
+    <url>
+        <loc>https://buildsmart.bg/privacy</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.3</priority>
+    </url>
+    <url>
+        <loc>https://buildsmart.bg/request-consultation</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.7</priority>
+    </url>
+</urlset>";
+    
+    await context.Response.WriteAsync(sitemapXml);
+});
+
 // Fetch and warm up localization cache from API
 try
 {
